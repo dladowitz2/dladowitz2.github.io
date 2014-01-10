372b1fcebfb19051ce529ad07d1663d9de5ded15
David Ladowitz <david@ladowitz.com>
2014-01-10 15:08:22 -0800

    initial commi

---
+++ resources/javascript.js
@@ -0,0 +1,7 @@
+// Your Javascript will go here!
+
+alert('hey Im in an external javasctipt page')
+alert('hey Im in an external javasctipt page')
+alert('hey Im in an external javasctipt page')
+alert('hey Im in an external javasctipt page')
+alert('hey Im in an external javasctipt page')
